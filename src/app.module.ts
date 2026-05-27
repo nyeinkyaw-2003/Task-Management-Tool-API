a13014eea3f8fc3b1cb3a0b36d5edc213cea11cf
Nyein Kyaw <nyeinkyaw@c3.com.mm>
2026-05-27 22:49:23 +0630

    migrate the old prisma to new one

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { ProjectModule } from './modules/project/project.module';
 import { UserModule } from './modules/user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [PrismaModule, UserModule, ProjectModule]
+  imports: [PrismaModule, UserModule, ProjectModule, AuthModule]
 })
 export class AppModule {}
